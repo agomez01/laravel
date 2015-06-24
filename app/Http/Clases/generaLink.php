622c798d4407a292b42f786ec5b2cdb055e0c9f3
ssilvac <sebastian.silva@hotmail.es>
2015-06-24 10:40:55 -0400

    clase generaLink
    se modifica la ruta del archivo "externo.php" en proyecto

--- app/Http/Clases/generaLink.php
+++ app/Http/Clases/generaLink.php
@@ -13,10 +13,21 @@
 	    	$sesion = Sesion::where('usuario_id', Session::get('idusuario'))->first();
 
 	    	if(!is_null($sesion)){
-	    		return URL_PLATAFORMA.'externo.php?idusuario='.Session::get('idusuario')."&modulo=$modulo&token=".$sesion->token;
+	    		return URL_PLATAFORMA.'externo/externo.php?idusuario='.Session::get('idusuario')."&modulo=$modulo&token=".$sesion->token;
 	    	}else{
 	    		return false;
 	    	}
 	    }
 
 	}
+
+
+	/* 
+
+	EJEMPLO USO
+
+	use App\Http\Clases\generaLink;
+	$link = generaLink::getLink('alumno');
+	return "<a href='".$link."'>Prueba</a>";
+
+	*/
