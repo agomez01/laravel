ee42b6234e8281473400130b5c21141d08a9e72e
Mitchel <mitchel.novoa@gmail.com>
2015-07-13 17:30:54 -0300

    test portada alumno

--- resources/views/alumno/home.blade.php
+++ resources/views/alumno/home.blade.php
@@ -11,13 +11,13 @@
 
 	
 <div class="row">
-    <div class="col-md-2 sidebar-left" >
-        
+    <div class="col-md-3 col-sm-4 sidebar-left" >        
     	<ul class="nav nav-pills nav-stacked">
-
-			  
-			  <li role="presentation"><a class="alu-reloj">00:00:00</a></li>
-			  <li role="presentation"><a class="alu-biblio" 	href="#">Biblioteca Alumno</a></li>
+    	<li class="reloj"><div id="reloj">00 : 00 : 00</div><div class="fecha"><img src="../assets/img/reloj.png" alt=""><script type="text/javascript"> var meses = new Array 
+    	("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"); 
+    	var diasSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"); 
+    	var f=new Date(); document.write(diasSemana[f.getDay()] + " " + f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear()); </script></div></li>
+			  <li role="presentation" ><a class="alu-biblio" 	href="#"><img src="../assets/img/biblioteca.png" alt="">Biblioteca Alumno</a></li>
 
 		</ul>
 
@@ -76,26 +76,28 @@
     </div>
 
 
-    <div class="col-md-9">
+    <div class="col-md-9 col-sm-8" >
     	<br><br><br>	
 
 		<div class="page-header">
 
 		<div class="pull-right form-inline">
-			<div class="btn-group">
-				<button class="btn btn-primary" data-calendar-nav="prev"><< Anterior</button>
-				<button class="btn" data-calendar-nav="today">Hoy</button>
-				<button class="btn btn-primary" data-calendar-nav="next">Próximo >></button>
+			<div class="btn-group controla-hoy">
+				<button class="btn controla" data-calendar-nav="prev"><img src="../assets/img/anterior.svg" alt="" width="20"></button>
+				<button class="btn hoy" data-calendar-nav="today">Hoy</button>
+				<button class="btn controla" data-calendar-nav="next"><img src="../assets/img/siguiente.svg" alt="" width="20"></button>
 			</div>
-			<div class="btn-group">
+			<div class="btn-group"><h3 class="h3-md"></h3></div>			
+			<div class="btn-group controla-mes">
 				<button class="btn btn-warning" data-calendar-view="year">Año</button>
 				<button class="btn btn-warning active" data-calendar-view="month">Mes</button>
 				<button class="btn btn-warning" data-calendar-view="week">Semana</button>
 				<button class="btn btn-warning" data-calendar-view="day">Día</button>
 			</div>
-		</div>
 
-		<h3></h3>		
+		</div>
+		<h3 class="h3-xs"></h3>
+				
 	</div>
 
     	<div id="calendar">calendario</div>
@@ -119,15 +121,15 @@
 		</div>
 
 			<div class="checkbox">
-				<label><input type="checkbox" id='ck_event_personal' class='ckb_filtro_eventos' checked> Eventos Personales</label>
+				<label><div class="ck_event_personal"><input type="checkbox" id='ck_event_personal' class='ckb_filtro_eventos' checked></div> Eventos Personales</label>
 			</div>
 			
 			<div class="checkbox">
-				<label><input type="checkbox" id='ck_event_utp' class='ckb_filtro_eventos' checked> Eventos Utp</label>
+				<label><div class="ck_event_global"><input type="checkbox" id='ck_event_global' class='ckb_filtro_eventos' checked></div> Eventos Globales</label>
 			</div>
 
 			<div class="checkbox">
-				<label><input type="checkbox" id='ck_event_sostenedor' class='ckb_filtro_eventos' checked> Eventos Sostenedor</label>
+				<label><div class="ck_event_curso"><input type="checkbox" id='ck_event_curso' class='ckb_filtro_eventos' checked></div> Eventos Curso</label>
 			</div>
 			
 		</div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -30,25 +30,58 @@
                     <span class="icon-bar"></span>
                 </button>
             </div>
+
+            <div class="top_line col-md-12 col-sm-12"><!-- top_line -->    
+    <div class="top_usuario" style="color:#fff;">    
+    </div>
+    <div class="top_btn" title="Salir">
+        <a href="#" >
+            Salir
+            <img src="../assets/img/webclass-btn-salir.png" alt="Salir" width="24" height="24"   />
+        </a>
+    </div>
+
+    <div class="top_btn" title="Ayuda">
+        <a href="http://webclass.com/tutoriales" target="_blank" >
+            Ayuda
+            <img src="../assets/img/webclass-btn-pregunta.png" alt="Ayuda" width="24" height="24"/>
+        </a>
+    </div>
+    <div class="top_btn" title="Soprte">
+        <a id="islpronto_link" href="mailto:soporte@webclass.com">
+            Chat Soporte
+            <img src="../assets/img/webclass-btn-soporte.png" alt=">Chat Soporte" name="islpronto_image" width="24" height="24" id="islpronto_image" style="border:none"/>
+        </a>
+    </div>
+    <div class="top_btn">
+        <a href="#" title="Cambiar tema">
+            Cambiar tema
+            <img src="../assets/img/webclass-btn-configuracion.png" alt="Cambiar tema"  width="24" height="24"/>
+        </a>
+    </div>
+    <div class="top_selector" title="Cambiar rol de usuario">
+        <img src="../assets/img/webclass-btn-cambiar_vista.png" width="24" height="24"/>
+        <span>    
+            <div id="cambioColegio">
+                <select name="comboboxcolegio" onChange="">
+                    <option  value="">Cambiar vista</option>         
+                    <option value=""></option>              
+                </select>
+            </div>
+        </span>     
+    </div>
+</div>
             
             <div class="navbar-inverse side-collapse in">            
                 <nav role="navigation" class="navbar-collapse">                    
                     <ul class="nav navbar-nav">                                    
-                        @yield('menu')                      
-                        <li class="dropdown" align="center">
-                            <a href="#" class="dropdown-toggle hvr-overline-from-left opciones" data-toggle="dropdown" >Opciones<b class="caret"></b></a>
-                            <ul class="dropdown-menu" id="dropdown-aux">      
-                                <li><a class="alumno" href="">Alumno</a></li>
-                                <li ><a class="chatsoporte" href="/login">Chat Soporte</a></li>
-                                <li ><a class="ayuda" href="/login">Ayuda</a></li>                                
-                                <li ><a class="salir" href="/logout">Salir</a></li>                                                    
-                            </ul>
-                        </li>                                
+                        @yield('menu')                                                 
                     </ul>
                 </nav>
             </div>
         </div>
-    </header>    
+    </header>  
+
     
        
     <div id="wrap">
@@ -68,5 +101,6 @@
             <script src="{{ asset('assets/js/calendar/calendar.js') }}" src="js/calendar.js"></script>
             <script src="{{ asset('assets/js/calendar/language/es-ES.js') }}" src="js/language/es-ES.js"></script>
             <script src="{{ asset('assets/js/calendar/app.js') }}" src="js/app.js"></script>
+            <script src="{{ asset('assets/js/reloj/reloj.js') }}" src="js/app.js"></script>
     </body>
 </html>
