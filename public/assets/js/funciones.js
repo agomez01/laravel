3e81a81f27cfdbc47620fd113093905ba8526844
Mitchel <mitchel.novoa@gmail.com>
2015-10-28 16:07:38 -0300

    WEB-3761 login lv no sigue linea de diseño

---
+++ public/assets/js/funciones.js
@@ -0,0 +1,651 @@
+$('document').ready(function(){
+
+
+		 $("#menu-toggle,#close-toggle").click(function(e) {
+	        e.preventDefault();
+	        $("#wrapper").toggleClass("toggled");
+
+
+	        $("#close-toggle2").toggleClass('cerrar-toggle2');
+	    });
+
+
+		 $("#close-toggle2").click(function(e) {
+	        e.preventDefault();
+	        $("#wrapper").toggleClass("toggled");
+
+	        $(this).toggleClass('cerrar-toggle2');
+	    });
+
+			$("#show").click(function(){
+			$("#recuperar").show();
+			});
+		});
+
+
+		obtenerEstadotest($("#idevaluacion").val());
+
+		paginar_preguntas();
+
+		mostrarFeed();//parseara las respuesta y mostrara el feed
+
+
+		$(document).on('click', '.eva-btnEnviar',  function(){
+			
+			var respuesta = new Array();
+
+			var div 			 = $(this).parents('div'); //obtengo la data desde el contenedor de la pregunta
+			var cadenaParametros = div.data('id'); //datos de pregunta que estoy respondiendo (test, pregunta, tipo)
+			var parametrosArray  = cadenaParametros.split('_'); //datos de la pregunta en un arreglo.
+
+			var test 			 = parametrosArray[0];
+			var pregunta 		 = parametrosArray[1];
+			var tipoPregunta     = parametrosArray[2];
+
+			
+			if(respuestaAlumno = obtenerRespuestaPregunta(test, pregunta, tipoPregunta,'0')){
+				enviarRespuesta(test, pregunta, tipoPregunta, respuestaAlumno);
+			}else{
+				return;
+			}
+
+			
+
+			obtenerFeedback(test, pregunta, tipoPregunta);
+		});
+
+		$(document).on('click', '#eva-btnEnviarTodo',  function(){
+
+			if (confirm("¿Estás seguro que deseas enviar todo y terminar?")){
+				
+				$("#eva-lanzaLoader").click(); //debe mostrar el loader
+
+				var cantPreg = 0;
+
+				$.each($('.eva-boxPregunta'), function (){
+					cantPreg++;
+				});
+
+				var test = $(this).attr('data-test');
+
+				var cant = 0;
+
+				$.each($('.eva-boxPregunta'), function (){
+
+						var cadenaParametros = $(this).data('id'); //datos de pregunta que estoy respondiendo (test, pregunta, tipo)
+						var parametrosArray  = cadenaParametros.split('_'); //datos de la pregunta en un arreglo.
+
+						//var test 			 = parametrosArray[0];
+						var pregunta 		 = parametrosArray[1];
+						var tipoPregunta     = parametrosArray[2];
+
+						
+						//console.log (test, pregunta, tipoPregunta);
+						if(respuestaAlumno = obtenerRespuestaPregunta(test, pregunta, tipoPregunta, '1')){
+							enviarRespuesta(test, pregunta, tipoPregunta, respuestaAlumno);
+						}						
+
+						cant++;
+
+						//console.log(cantPreg, cant);
+						console.log(respuestaAlumno);
+				});
+
+				finalizarLaEvaluacion();
+			}
+		});
+
+
+		$(document).on('click', '#btn_pausar_salir', function(){
+
+			if (confirm("¿Estás seguro que pausar la evaluación?")){
+				
+				$("#eva-lanzaLoader").click(); //debe mostrar el loader
+
+				var cantPreg = 0;
+
+				$.each($('.eva-boxPregunta'), function (){
+					cantPreg++;
+				});
+
+				var test = $(this).attr('data-test');
+
+				var cant = 0;
+
+				$.each($('.eva-boxPregunta'), function (){
+
+						var cadenaParametros = $(this).data('id'); //datos de pregunta que estoy respondiendo (test, pregunta, tipo)
+						var parametrosArray  = cadenaParametros.split('_'); //datos de la pregunta en un arreglo.
+
+						//var test 			 = parametrosArray[0];
+						var pregunta 		 = parametrosArray[1];
+						var tipoPregunta     = parametrosArray[2];
+
+						
+						//console.log (test, pregunta, tipoPregunta);
+						if(respuestaAlumno = obtenerRespuestaPregunta(test, pregunta, tipoPregunta, '1')){
+							enviarRespuesta(test, pregunta, tipoPregunta, respuestaAlumno);
+						}						
+
+						cant++;
+
+						//console.log(cantPreg, cant);
+						console.log(respuestaAlumno);
+				});
+
+				transcurrido();
+			}
+
+		});
+
+
+	$(document).on('click', '#btn_continuar', function(){
+
+		var test = $("#idevaluacion").val();
+
+		$.ajax({
+			url: '/evaluacion/'+test+"/7",
+			async: true,
+			type: "get",
+			data: {'test': test },
+			dataType: 'json',
+			success: function(data){
+				
+				if (data.estado){
+					console.log("continuar...");
+				}			
+
+			}
+		});
+
+	});
+});
+
+
+function mostrarFeed(){
+
+	$.each($('.eva-boxPreguntaResp'), function (){
+		var datos = $(this).attr('data-id');
+		datos = datos.split('_');
+		obtenerFeedback( datos[0], datos[1], datos[2] ); 
+	});
+
+}
+
+function obtenerFeedback(test, pregunta, tipo){
+
+		$.ajax({
+			url: '/evaluacion/feed/'+test+"/"+pregunta+"/"+tipo,
+			async: false,
+			type: "get",
+			dataType: 'json',
+			success: function(data){
+
+				if (data.estado){
+
+					var div = "div[data-id='"+data.div+"']";
+					$(div+" > .eva-casillaCuerpo").remove(); //removemos las respuestas
+
+					// procedemos a mostrar el feedback
+
+					if(data.respuesta != ''){
+						$(div).append("<div class='alert alert-warning feedr'><strong>Respuesta:</strong> "+data.respuesta+"</div>");
+					}
+					/*if(data.comentario != ''){
+						$(div).append("<div class='alert alert-info'><strong>Comentario:</strong> "+data.comentario+"</div>");
+					}*/
+
+					if(data.correccion != ''){
+						$(div).append("<div class='alert alert-warning'><strong>Corrección:</strong> "+data.correccion+"</div>");
+					}
+
+					if(data.puntuacion != ''){
+						$(div).append("<div class='alert alert-success'><strong>Puntuación:</strong> "+data.puntuacion+"</div>");
+					}
+
+					if(data.retro != ''){
+						$(div).append("<div class='alert alert-success'><strong>Retroalimentación General:</strong> "+data.retro+"</div>");
+					}
+					
+					parserImg();
+
+				}			
+
+			}
+		});
+	}
+
+function transcurrido(){
+
+	var test = $("#idevaluacion").val();
+
+	$.ajax({
+		url: '/evaluacion/'+test+"/6",
+		async: true,
+		type: "get",
+		data: {'test': test },
+		dataType: 'json',
+		success: function(data){
+			
+			if (data.estado){
+				location.href = "/home";
+			}			
+
+		}
+	});
+}
+
+
+
+function finalizarLaEvaluacion(){
+
+	var test = $("#idevaluacion").val();
+
+	$.ajax({
+		url: '/evaluacion/'+test+"/4",
+		async: true,
+		type: "get",
+		data: {'test': test },
+		dataType: 'json',
+		success: function(data){
+			
+			if (data.estado){
+				location.href = "/home";
+			}			
+
+		}
+	});
+}
+
+function enviarRespuesta(test, pregunta, tipoPregunta, respuestaAlumno){
+
+		var form 		= $('#resp_'+pregunta); //form para llamada ajax
+		var token 		= $('#resp_'+pregunta+' input').val();
+		var dataForm 	= form.serialize(); //datos del form para llamada ajas serializados.
+		var urlPost 	= form.attr('action');
+
+		$.ajax({
+			url: urlPost,
+			async: false,
+			type: "post",
+			data: {'_token': token, 'test':test, 'pregunta': pregunta, 'tipoPregunta':tipoPregunta, 'respuestaAlumno':respuestaAlumno },
+			success: function(resp){
+				sendResposeMessenge(resp,pregunta, test, tipoPregunta);
+			}
+	    });
+
+}
+
+function sendResposeMessenge(resp, idpregunta, test, tipoPregunta){
+	//console.log(resp,idpregunta);
+	idResp = resp.last_insert_id;
+	if (idResp > 0){
+		//alert("enviada"+idResp);
+		$("#"+test+"_"+idpregunta+"_"+tipoPregunta).removeClass("eva-boxPregunta").addClass("eva-boxPreguntaResp");
+
+		$("#respMessege"+idpregunta).html(resp.messege);
+		$("#eva-minPregNum"+idpregunta).css(  'background-color', '#0BE409');
+	}else{
+		console.log(resp.messege);
+	}
+}
+
+function obtenerRespuestaPregunta(test, pregunta, tipoPregunta, todo){
+		
+		switch(tipoPregunta){
+
+				case '1':
+
+						var name = 'vof_'+test+pregunta;
+						var resp = $('input:radio[name='+name+']:checked').val();
+						
+						if (resp === "" || typeof resp === "undefined")
+						{
+								if (todo === '1'){
+
+										var respuestaAlumno = 2;
+
+								}else{
+
+										if (confirm("¿Estás seguro que deseas enviar sin responder la pregunta?")){
+
+											var respuestaAlumno = 2;
+
+										}else{
+
+											return false;
+
+										}
+								}
+
+						}else{
+
+								respuestaAlumno = resp;
+
+						}
+
+					break;
+				case '2':
+						//emparejamiento
+						var clase = 'eva-RespEmpareja'+pregunta;
+						var todasSinResp = true;
+						var algunaSinResp = false;
+						//alert(clase);
+						var parejasArray = new Array();
+						var x=0;
+						$.each( $("."+clase), function (){
+
+							var pareja = new Array();
+
+						    var col1 = $(this).attr('id');
+						    var col2 = $('#'+col1+' option:selected').val();
+
+						    pareja[0] = col1;
+						    pareja[1] = col2;
+
+						    if(col2 != 0){
+						    	todasSinResp = false;
+						    }
+
+						    if (col2 == 0){
+						    	algunaSinResp = true;
+						    }
+
+						    
+						    parejasArray[x] = JSON.stringify(pareja);
+						    x++;
+						});
+							
+							if(todasSinResp)
+							{
+
+								if (todo === '1'){
+
+										respuestaAlumno = JSON.stringify(parejasArray);
+
+								}else{
+
+										if (confirm("¿Estás seguro que deseas enviar sin responder la pregunta?")){
+
+											respuestaAlumno = JSON.stringify(parejasArray);
+
+										}else{
+
+											return;
+
+										}
+								}
+
+							}else{
+
+								if(algunaSinResp){
+
+									if (todo === '1'){
+
+											respuestaAlumno = JSON.stringify(parejasArray);
+
+									}else{
+
+											if (confirm("Falta una o más parejas que vincular. ¿Seguro deseas enviar la repuesta?")){
+
+												respuestaAlumno = JSON.stringify(parejasArray);
+
+											}else{
+
+												return;
+
+											}
+									}
+
+
+								}else{
+
+									respuestaAlumno = JSON.stringify(parejasArray);
+									
+								}
+							}
+
+					break;
+				case '3':
+						
+						respuestaAlumno = $("#des_"+test+pregunta).val();
+
+						if (respuestaAlumno === ""){
+
+							if (todo === '0'){
+
+									if (!confirm("¿Estás seguro que deseas enviar sin responder la pregunta?")){
+
+										return;
+										
+									}
+
+							}
+
+						}
+						
+					break;	
+				case '4':
+						//respuesta alternativa
+						var name = 'respAlt_'+test+pregunta;
+						var resp = $('input:radio[name='+name+']:checked').val();
+						
+						if (resp === "" || typeof resp === "undefined"){
+
+							if (todo === '1'){
+
+									var respuestaAlumno = 0;
+
+							}else{
+
+									if (confirm("¿Estás seguro que deseas enviar sin responder la pregunta?")){
+
+										var respuestaAlumno = 0;
+
+									}else{
+
+										return;
+
+									}
+							}
+
+						}else{
+
+							respuestaAlumno = resp;
+
+						}
+					break;		
+				case '5':
+						
+						respuestaAlumno = $("#cor_"+test+pregunta).val();
+
+						if (respuestaAlumno === ""){
+
+							if (todo === '0'){
+
+									if (!confirm("¿Estás seguro que deseas enviar sin responder la pregunta?")){
+
+										return;
+
+									}
+
+							}
+
+							
+
+						}
+
+					break;	
+			}
+
+			return respuestaAlumno;
+
+}
+
+
+
+
+function obtenerEstadotest(test){// variable 'test' en Base64
+
+	var token = $("#tokenEvaluacion").val();
+
+	$.ajax({
+		url: '/evaluacion/'+test+"/3",
+		async: true,
+		type: "get",
+		data: {'test': test },
+		dataType: 'json',
+		success: function(data){
+			
+			console.log(data);
+
+		}
+	});
+
+}
+
+	function comenzar_evaluacion(test){// variable 'test' en Base64
+
+	}
+
+
+	// lo creamos como variable global !!!
+	var preg 	= new Array();				// array multidimencional
+	var paginas;
+
+
+
+
+	function paginar_preguntas(){
+
+		var num 	= $("#porpagina").val();	// cantidad de pregutas por página
+
+		if(num != 0){
+
+			var cont 	= 0;						// contador de preguntas
+			var pag 	= 1;						// número de página 
+
+			// obtenemos la cantidad de preguntas
+			var nump = $(".div_pregunta").length;
+
+
+			// Calculamos la cantidad de páginas
+			paginas = Math.ceil(nump / num);
+
+			if(paginas > 0){
+				// creamos la matriz con los array dependiendo de la cantidad de paginas
+				for (var i = 1; i <= paginas; i++) {
+					preg[i] = new Array();
+				};
+
+
+				// creamos un array multidimencional, separando las preguntas por página
+				$.each($(".div_pregunta"), function(){
+					preg[pag][cont] = $(this).attr('data-id');
+					cont++;
+
+					if(cont == num ){
+						cont = 0;
+						pag ++;
+					}
+
+				});
+			}
+
+			// mostramos la pagina actual y la cantidad total Ejem: 1/6
+			$("#num_pag").text("1/"+paginas);
+
+			paginar();
+
+		}else{
+			$("#navegacion").hide();
+		}
+
+
+	}
+
+	function paginar(){
+
+		var num 	= $("#porpagina").val();	// cantidad de pregutas por págin
+		
+		var pagina_actual	= $("#pag").val();
+
+		// ocultamos todas las preguntas
+		$(".div_pregunta").hide();
+
+		// luego mostramos solo las preguntas correspondientes
+		// a la página seleccionada
+		$.each(preg[pagina_actual], function(i,v){
+			$(".div_pregunta[data-id='"+v+"']").show();
+		});
+
+		if(paginas == 1){
+			$("#pag_sig").attr('disabled', true);
+		}
+
+	}
+
+	$(document).on('click', '#pag_ant', function(){
+
+		//obtenemos la página actual
+		var pag = parseInt($("#pag").val());
+
+		pag--;
+
+		$("#num_pag").text(pag+"/"+paginas);
+		$("#pag").val(pag);
+
+		// ejecutamos la paginación
+		paginar();
+
+		// si alcanzamos el mínimo de paginas deshabilitamos el botón
+		if(pag == 1){
+			$(this).attr('disabled', true);
+		}
+
+		// al restar una página nos aseguramos siempre de habilitar el botón siguiente !
+		$('#pag_sig').attr('disabled', false);
+
+	});
+
+	$(document).on('click', '#pag_sig', function(){
+
+		//obtenemos la página actual
+		var pag = parseInt($("#pag").val());
+
+		pag++;
+		
+		$("#num_pag").text(pag+"/"+paginas);
+		$("#pag").val(pag);
+
+		// ejecutamos la paginación
+		paginar();
+
+		// si alcanzamos el tope de paginas deshabilitamos el botón
+		if(pag == paginas){
+			$(this).attr('disabled', true);
+		}
+
+		// al sumar una página nos aseguramos siempre de habilitar el botón anterior !
+		$('#pag_ant').attr('disabled', false);
+
+	});	
+
+
+	function parserImg(){
+		/*
+		se parsearan las imagenes que no contengan una dirección completa a recursos
+		*/
+		var url_recursos = $("#url_recursos").val();
+
+		$("img").each(function(i){
+
+			var url = $(this).attr('src');
+			var c = url.search(/http/i);
+
+			if(c < 0){
+				$(this).attr('src', url_recursos+url);
+			}
+
+		});
+	}
--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -13,19 +13,63 @@
 
     <script src="{{ asset('assets/js/jquery-2.1.4.min.js') }}"></script>
     <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-    
+    <script src="{{ asset('assets/js/jquery-1.11.3.min.js') }}"></script>
+      <script src="{{ asset('assets/js/funciones.js') }}"></script>
+<script>
+    $(document).ready(function(){    
+            $("#show").click(function(){
+            $("#recuperar").show();
+            });
+        });
+</script>   
+  
   </head>
-  <body>
-    
-    <br><br><br><br><br><br>
+  <body style="background:#F4F4F4;">  
+<div class="collapse navbar-collapse">
 
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3">
+    <nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="">
+                <img alt="" src="../../assets/img/isotipo_mono.svg" width="25">
+            </a>
+        </div>
+        <ul class="nav navbar-nav header-nav">
+            <li ><a href="http://webclass.com" target="_blank">webclass.com</a></li>
+            <li><a href="http://www.webclass.com/tutoriales/inicio/" target="_blank">Capacitación</a></li>
+            <li><a href="#" onclick="javascript:window.open('http://proyecto.webescuela.cl/sistema/testing/soporte.php', '' , 'width=700 , height=800' , true)">Soporte</a></li>
+        </ul>
+    </nav>
+</div>
+ <div class="col-md-12 box-alertas" width="300px">
+            <div class="alert alert-success password-login" id="recuperar" style="display:none;">
+                <button type="button" class="close"  data-dismiss="alert">&times;</button>
+                <p>Ingrese nombre de usuario y presione 'Olvidé mi contraseña'. Le enviaremos la contraseña a su correo.</p>
+                <a href="http://www.webclass.com/tutoriales/recuperar-contrasena-de-usuario/" target="_blank">Ver video explicativo</a>
+            </div>
+                {{-- Preguntamos si hay algún mensaje de error y si hay lo mostramos  --}}               
+
+                @if ($errors->any())
+                  <div class="alert alert-danger error-login">
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    <strong>Por favor corrige los siguentes errores:</strong>
+                    <ul>
+                  @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                  @endforeach
+                    </ul>                            
+                  </div>
+                @endif
+        </div>
+
+    <div class="row-fluid contenedor-login col-sm-12 col-xs-12" >
+
+        <div class="col-md-3 col-sm-12 col-xs-12 col-md-offset-5 box-login">
             <div class="panel panel-login">
                 <div class="panel-heading">
                     <div class="row">
                         <div class="col-xs-12">
-                            <a href="#" class="active" id="login-form-link">Login WebClass</a>
+                            <a href="#" class="active" id="login-form-link"><img src="assets/img/logo_flat.svg" width="180"></a>
                         </div>
                     </div>
                     <hr>
@@ -34,57 +78,35 @@
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-lg-12">
-                            <form id="login-form" action="{{ URL::to('login') }}" method="post" role="form" style="display: block;">
+                            <form  id="login-form" action="{{ URL::to('login') }}" method="post" role="form" style="display: block;">
                                 <div class="form-group">
-                                    <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Usuario" value="">
+                                    <input type="text" name="username" id="username" tabindex="1" class="form-control box-input-1" placeholder="Usuario" value="">
+                                    <a class="img-input-1"></a>
                                 </div>
                                 <div class="form-group">
-                                    <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Contraseña">
+                                    <input type="password" name="password" id="password" tabindex="2" class="form-control box-input-2" placeholder="Contraseña">
+                                    <span class="img-input-2"></span>
                                 </div>
-
-
-                                
-                                {{-- Preguntamos si hay algún mensaje de error y si hay lo mostramos  --}}
 
                                 @if(Session::has('mensaje_error'))
-                                    <div class="alert alert-warning">
-                                        {{ Session::get('mensaje_error') }}
+                                    <div class="alert alert-danger error-login">
+                                {{ Session::get('mensaje_error') }}
                                     </div>                                        
                                 @endif
-
-                                @if ($errors->any())
-                                  <div class="alert alert-danger">
-                                    <button type="button" class="close" data-dismiss="alert">&times;</button>
-                                    <strong>Por favor corrige los siguentes errores:</strong>
-                                    <ul>
-                                      @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                      @endforeach
-                                    </ul>
-                                  </div>
-                                @endif
-
+                        
                                 <!--
                                 <div class="form-group text-center">
                                     <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
                                     <label for="remember">Recordar contraseña</label>
                                 </div>
                             -->
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col-sm-6 col-sm-offset-3">
-                                            <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Entrar">
-                                        </div>
+                                <div class="form-group">                                                                                                                                                                           
+                                    <div class="text-center" id="show" >
+                                        <a href="#"  tabindex="5" >Olvidé mi contraseña</a>                                                
                                     </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col-lg-12">
-                                            <div class="text-center">
-                                                <a href="http://phpoll.com/recover" tabindex="5" class="forgot-password">olvidé mi contraseña</a>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <div>
+                                         <input class=" btn-success btn-entrar" type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Entrar">
+                                    </div>                             
                                 </div>
 
                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -93,6 +115,21 @@
                     </div>
                 </div>
             </div>
+        </div>
+       
+ 
+    </div>
+
+    <div class=" col-md-12 row box-footer-1">       
+        <div class="col-md-9 box-footer-2">
+            <div class="img-pie-1">
+                <img src="../../assets/img/support.svg" width="55" alt="">
+            </div> 
+            <div class="texto-pie-1">
+                <span>SERVICIO ATENCIÓN AL CLIENTE</span>
+                <p>Tel.: strong(+56) 22 869 9100 <br>
+                E-mail: sac@webclass.com</p>
+            </div>            
         </div>
     </div>
 
